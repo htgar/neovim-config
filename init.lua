1862f1a45a832cd1070a7b61b6d88c1724d21fbe
Htgar <development@htgar.org>
2024-07-08 12:37:28 +0800

    Use FzfLua to replace netrw

--- init.lua
+++ init.lua
@@ -169,6 +169,37 @@ require("lazy").setup({
 			require("fzf-lua").setup({})
 			nmap_leader("ff", "<Cmd>FzfLua files<CR>", "[F]uzzy")
 			nmap_leader("fg", "<Cmd>FzfLua live_grep<CR>", "[G]rep")
+
+			-- Use FzfLua as default file explorer
+			local find_files_hijack_netrw = vim.api.nvim_create_augroup("find_files_hijack_netrw", { clear = true })
+			-- clear FileExplorer appropriately to prevent netrw from launching on folders
+			-- netrw may or may not be loaded before telescope-find-files
+			-- conceptual credits to nvim-tree and telescope-file-browser
+			vim.api.nvim_create_autocmd("VimEnter", {
+				pattern = "*",
+				once = true,
+				callback = function()
+					pcall(vim.api.nvim_clear_autocmds, { group = "FileExplorer" })
+				end,
+			})
+			vim.api.nvim_create_autocmd("BufEnter", {
+				group = find_files_hijack_netrw,
+				pattern = "*",
+				callback = function()
+					vim.schedule(function()
+						-- Early return if netrw or not a directory
+						if vim.bo[0].filetype == "netrw" or vim.fn.isdirectory(vim.fn.expand("%:p")) == 0 then
+							return
+						end
+
+						vim.api.nvim_buf_set_option(0, "bufhidden", "wipe")
+
+						require("fzf-lua").files({
+							cwd = vim.fn.expand("%:p:h"),
+						})
+					end)
+				end,
+			})
 		end,
 	},
 
