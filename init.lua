d0980a468e6e66075348e0d9c16a761bd3de28d7
Htgar <development@htgar.org>
2024-07-09 20:38:53 +0800

    Added flash.nvim

--- init.lua
+++ init.lua
@@ -120,6 +120,54 @@ require("lazy").setup({
 	"tpope/vim-rsi",
 
 	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+			{
+				"S",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+			{
+				"R",
+				mode = { "o", "x" },
+				function()
+					require("flash").treesitter_search()
+				end,
+				desc = "Treesitter Search",
+			},
+			{
+				"<c-s>",
+				mode = { "c" },
+				function()
+					require("flash").toggle()
+				end,
+				desc = "Toggle Flash Search",
+			},
+		},
+	},
+
+	{
 		"aserowy/tmux.nvim",
 		config = function()
 			require("tmux").setup({
